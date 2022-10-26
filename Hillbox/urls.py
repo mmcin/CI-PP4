99af838b730c8eb61f41552a892c3d5175b4b9d0
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:38:03 +0100

    Adds EditSite to urls.py

--- Hillbox/urls.py
+++ Hillbox/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('not_authorised', views.NotAuthorised, name='not_authorised'),
     path('sites/', views.SiteList.as_view(), name='sites'),
     path('gallery/', views.PhotoList.as_view(), name='gallery'),
+    path('edit/<site_id>', views.EditSite, name='edit'),
 ]
