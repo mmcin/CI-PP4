5058f8536dd3cec6b720c6968a9f5ddf01653eed
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:51:36 +0100

    Adds DeleteGallery to urls.py

--- Hillbox/urls.py
+++ Hillbox/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path('gallery/', views.PhotoList.as_view(), name='gallery'),
     path('gallery/<slug:slug>/', views.PhotoDetail.as_view(), name='photo_detail'),
     path('gallery_upload/', views.UploadGalleryImage, name='gallery_upload'),
+    path('gallery/delete/<uploaded_by>/<photo_id>', views.DeleteGallery, name='delete_gallery'),
     path('edit/<site_id>', views.EditSite, name='edit'),
     path('delete/<site_id>', views.DeleteSite, name='delete'),
     path('sites/<slug:slug>/', views.SiteDetail.as_view(), name='site_detail'),
     path('site_comment_edit/<comment_id>', views.EditComment, name='site_comment_edit'),
     path('site_comment_delete/<comment_id>', views.DeleteComment, name='site_comment_delete'),
+    
       
 ]
