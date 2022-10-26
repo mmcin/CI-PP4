18a33e73a6c9119c2473d0830c5b1d68f8cb4be0
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:47:50 +0100

    Adds PhotoDetail to url

--- Hillbox/urls.py
+++ Hillbox/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('not_authorised', views.NotAuthorised, name='not_authorised'),
     path('sites/', views.SiteList.as_view(), name='sites'),
     path('gallery/', views.PhotoList.as_view(), name='gallery'),
+    path('gallery/<slug:slug>/', views.PhotoDetail.as_view(), name='photo_detail'),
     path('edit/<site_id>', views.EditSite, name='edit'),
     path('delete/<site_id>', views.DeleteSite, name='delete'),
     path('sites/<slug:slug>/', views.SiteDetail.as_view(), name='site_detail'),
