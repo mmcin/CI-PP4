b0b571e2615b5778d8824a79922392962457a4f7
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 15:45:46 +0100

    Adds GalleryUpload form

--- Hillbox/forms.py
+++ Hillbox/forms.py
@@ -6,3 +6,9 @@ class SiteUpload(forms.ModelForm):
     class Meta:
         model = FlyingSite
         fields = ('site_name', 'wind_direction', 'overview', 'landing_information', 'warnings', 'featured_image', 'status', 'slug')
+
+# this is the form to upload a gallery images
+class GalleryUpload(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ('featured_image', 'site_name', 'status', 'slug')
