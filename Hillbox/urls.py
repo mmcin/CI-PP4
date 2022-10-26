d668c5dc7c6c46ae51cd3e2263ea8e367453afc5
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:34:55 +0100

    Adds PhotoList to urls.py

--- Hillbox/urls.py
+++ Hillbox/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('not_authorised', views.NotAuthorised, name='not_authorised'),
     path('sites/', views.SiteList.as_view(), name='sites'),
+    path('gallery/', views.PhotoList.as_view(), name='gallery'),
 ]
