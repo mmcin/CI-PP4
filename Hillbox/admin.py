5e526b8b4b6fb853584ecdf71fa3df087bc3ebb1
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:26:40 +0100

    Adds FlyingSiteAdmin to admin.py

--- Hillbox/admin.py
+++ Hillbox/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import FlyingSite, Comment, Photo, PhotoComment
+
+# this is the admin for the FlyingSite 
+models@admin.register(FlyingSite)
+class FlyingSiteAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('site_name',)}
+    actions = ['approve_sites']
+    list_display = ('site_name', 'updated_on', 'approved', 'pilot')
+    search_fields = ['site_name', 'wind_direction']
+    list_filter = ('approved', 'updated_on')
