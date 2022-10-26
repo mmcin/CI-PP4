ff3b8d7c6ac1871ff15bd24b396298d7c9e25053
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:29:13 +0100

    Adds PhotoAdmin to admin.py and approval methods

--- Hillbox/admin.py
+++ Hillbox/admin.py
@@ -12,3 +12,18 @@ class FlyingSiteAdmin(admin.ModelAdmin):
     list_display = ('site_name', 'updated_on', 'approved', 'pilot')
     search_fields = ['site_name', 'wind_direction']
     list_filter = ('approved', 'updated_on')
+    
+    def approve_flying_sites(self, request, queryset):
+        queryset.update(approved=True)
+
+# this id the admin for the gallery images/photos
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('site_name',)}
+    actions = ['approve_photos']
+    list_display = ('site_name', 'updated_on', 'approved')
+    search_fields = ['site_name', 'wind_direction']
+    list_filter = ('approved', 'updated_on')
+    
+    def approve_photos(self, request, queryset):
+        queryset.update(approved=True)
