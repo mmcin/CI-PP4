cbf6f026a71ec27e8d10a867cfb970a19bb52f2d
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 15:34:42 +0100

    Adds flying site model and fields

--- Hillbox/models.py
+++ Hillbox/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+# this is where I will store the flying sites that user uploads I based this model on the model from the CI blog post with the obvious changes such as adding choices for the wind direction
+class FlyingSite(models.Model):
+
+    WIND_CHOICES = (
+        ('N', 'N'),
+        ('NW', 'NW'),
+        ('NE', 'NE'),
+        ('S', 'S'),
+        ('SE', 'SE'),
+        ('SW', 'SW'),
+        ('E', 'E'),
+        ('W', 'W'),)
+    site_name = models.CharField(max_length = 150, unique=True)
+    wind_direction = models.CharField(max_length = 150, choices=WIND_CHOICES)
+    slug = models.SlugField(max_length=200, unique=True, default = "12345")
+    pilot = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flying_sites")
+    updated_on = models.DateTimeField(auto_now=True)
+    overview = models.TextField()
+    landing_information = models.TextField()
+    warnings = models.TextField()
+    featured_image = CloudinaryField('images', default='placeholder')
+    status = models.IntegerField(choices = STATUS, default = 0)
+    approved = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ["-updated_on"]
+
+    def __str__(self):
+        return self.site_name
+   
