835d6c973c98331292b6fe040f18532e4cd45b52
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 16:00:06 +0100

    index added to views.py

--- Hillbox/views.py
+++ Hillbox/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic, View
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import FlyingSite, Photo, Comment, PhotoComment
+from .forms import CommentForm, PhotoCommentForm, ContactForm, SiteUpload, GalleryUpload
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+import os
+if os.path.isfile('env.py'):
+    import env
+
+# this view returns the homepage
+def index(request):
+    return render(
+        request,
+        "index.html",
+    )
