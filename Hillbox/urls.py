cd26a75a3a07ff2c91a7fc9baf493f39366f1325
martin-mcinerney <martin_mcinerney@live.ie>
2022-10-26 17:50:01 +0100

    Adds UploadGalleryImage to urls.py

--- Hillbox/urls.py
+++ Hillbox/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('sites/', views.SiteList.as_view(), name='sites'),
     path('gallery/', views.PhotoList.as_view(), name='gallery'),
     path('gallery/<slug:slug>/', views.PhotoDetail.as_view(), name='photo_detail'),
+    path('gallery_upload/', views.UploadGalleryImage, name='gallery_upload'),
     path('edit/<site_id>', views.EditSite, name='edit'),
     path('delete/<site_id>', views.DeleteSite, name='delete'),
     path('sites/<slug:slug>/', views.SiteDetail.as_view(), name='site_detail'),
     path('site_comment_edit/<comment_id>', views.EditComment, name='site_comment_edit'),
     path('site_comment_delete/<comment_id>', views.DeleteComment, name='site_comment_delete'),
+      
 ]
