ab6cbd9f31904809b3595a1f9f1bbc5e67f6dcd9
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-01 16:04:11 +0000

    Forms.py pep8 approved

--- Hillbox/forms.py
+++ Hillbox/forms.py
@@ -2,32 +2,52 @@ from .models import Comment, PhotoComment, FlyingSite, Photo
 from django import forms
 
 # this is the upload form for flying sites
+
+
 class SiteUpload(forms.ModelForm):
     class Meta:
         model = FlyingSite
-        fields = ('site_name', 'wind_direction', 'overview', 'landing_information', 'warnings', 'featured_image', 'status', )
+        fields = (
+            'site_name',
+            'wind_direction',
+            'overview',
+            'landing_information',
+            'warnings',
+            'featured_image',
+            'status',
+     )
 
 # this is the form to upload a gallery images
+
+
 class GalleryUpload(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ('featured_image', 'site_name', 'status',)
 
 # this is the form to comment on a flying site
+
+
 class CommentForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = Comment
         fields = ('body',)
-        
+
 
 # this is the form to comment on a gallery image
 class PhotoCommentForm(forms.ModelForm):
     class Meta:
         model = PhotoComment
         fields = ('body',)
-        body = forms.CharField(widget = forms.Textarea(attrs={"rows":10, "cols":25}))
+        body = forms.CharField(
+            widget = forms.Textarea(
+                attrs = {
+                    "rows": 10,
+                    "cols": 25}))
 
 # this is the contact form
+
+
 class ContactForm(forms.Form):
     first_name = forms.CharField(max_length = 50)
     email_address = forms.EmailField(max_length = 150)
