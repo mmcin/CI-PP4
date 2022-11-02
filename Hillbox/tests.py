3d8f6c893d952fa366809d897998061b1e011fd4
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-02 09:48:25 +0000

    Added upload form automated testing

--- Hillbox/tests.py
+++ Hillbox/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .forms import SiteUpload, GalleryUpload
 
-# Create your tests here.
+# class TestDjango(TestCase):
+
+#     def test_this_thing_works(self):
+#         self.assertEqual(1,0)
+
+class TestSiteUploadForm(TestCase):
+    def test_site_upload_form(self):
+        form = SiteUpload({'site_name': "", 'pilot': "jim", 'wind_direction': "N", 'overview':'gjhg', 'landing_information':'some text', 'warnings':'some text', 'featured_image':"", 'status':1})
+        self.assertFalse(form.is_valid())
+        self.assertIn('site_name', form.errors.keys())
+        self.assertEqual(form.errors['site_name'][0], 'This field is required.')
+
+class TestGalleryForm(TestCase):
+    def test_gallery_upload_form(self):
+        form = GalleryUpload({'featured_image':"", 'site_name':"", 'status':1,})
+        self.assertFalse(form.is_valid())
+        self.assertIn('site_name', form.errors.keys())
+        self.assertEqual(form.errors['site_name'][0], 'This field is required.')
